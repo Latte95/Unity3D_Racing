89736134436d2e3a0fc6114b8af694e2777207d0
Latte95 <123979439+Latte95@users.noreply.github.com>
2023-06-28 10:18:22 +0900

    test

--- Assets/3.Script/Kart/Kart.cs
+++ Assets/3.Script/Kart/Kart.cs
@@ -5,17 +5,15 @@ using UnityEngine;
 [System.Serializable]
 public class AxleInfo
 {
-    public WheelCollider[] leftWheel = new WheelCollider[2];
-    public WheelCollider[] rightWheel = new WheelCollider[2];
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
     public bool motor;
     public bool steering;
 
-    public AxleInfo(WheelCollider l1, WheelCollider l2, WheelCollider r1, WheelCollider r2, bool mo, bool st)
+    public AxleInfo(WheelCollider lw, WheelCollider rw, bool mo, bool st)
     {
-        leftWheel[0] = l1;
-        leftWheel[1] = l2;
-        rightWheel[0] = r1;
-        rightWheel[1] = r2;
+        leftWheel = lw;
+        rightWheel = rw;
         motor = mo;
         steering = st;
     }
@@ -25,13 +23,13 @@ public class Kart : MonoBehaviour
 {
     [Header("Status")]
     [SerializeField]
-    public  float speed = 400.0f;
+    public float speed = 400.0f;
     [SerializeField]
-    public  float maxSpeed = 10f;
+    public float maxSpeed = 10f;
     [SerializeField]
     public readonly float boostSpeed = 1.3f;
     [SerializeField]
-    public  float steerRotate = 10.0f;
+    public float steerRotate = 10.0f;
 
     [Header("Tires")]
     [HideInInspector]
@@ -41,9 +39,10 @@ public class Kart : MonoBehaviour
     [HideInInspector]
     public GameObject[] wheels_Col_Obj;
     public WheelFrictionCurve nomalFriction_f { get; private set; }
-    public WheelFrictionCurve driftFriction_f; 
+    public WheelFrictionCurve driftFriction_f;
     public WheelFrictionCurve nomalFriction_s { get; private set; }
     public WheelFrictionCurve driftFriction_s;
+    public WheelFrictionCurve frontWheelSideFriction;
     public List<AxleInfo> axleInfos;
     private GameObject[] wheels;
     public float vehicleWidth { get; private set; }
@@ -73,25 +72,26 @@ public class Kart : MonoBehaviour
         SetAxelInfo();
 
         // 휠 콜라이더 위치를 바퀴 오브젝트(이미지) 위치로 동기화
-        for (int i = 0; i < wheels_Mesh.Length; i++)
-        {
-            wheels[i].transform.position = wheels_Mesh[i].transform.position;
-        }
+        //for (int i = 0; i < wheels_Mesh.Length; i++)
+        //{
+        //    wheels[i].transform.position = wheels_Mesh[i].transform.position;
+        //}
         vehicleWidth = Mathf.Abs(wheels[0].transform.position.magnitude - wheels[1].transform.position.magnitude);
         wheelBase = Mathf.Abs(wheels[0].transform.position.magnitude - wheels[2].transform.position.magnitude);
 
         // 드리프트를 위한 바퀴 마찰력 세팅을 위한 변수 초기화
-        nomalFriction_f = wheels_Col[0].forwardFriction;
-        driftFriction_f = wheels_Col[0].forwardFriction;
-        driftFriction_f.stiffness = driftFriction;
-        nomalFriction_s = wheels_Col[0].sidewaysFriction;
-        driftFriction_s = wheels_Col[0].sidewaysFriction;
-        driftFriction_s.stiffness = driftFriction;
+        nomalFriction_f = wheels_Col[3].forwardFriction;
+        driftFriction_f = wheels_Col[3].forwardFriction;
+        driftFriction_f.stiffness = nomalFriction_f.stiffness * driftFriction;
+        nomalFriction_s = wheels_Col[3].sidewaysFriction;
+        driftFriction_s = wheels_Col[3].sidewaysFriction;
+        driftFriction_s.stiffness = nomalFriction_s.stiffness * driftFriction;
+        frontWheelSideFriction = wheels_Col[0].sidewaysFriction;
     }
 
     private void SetAxelInfo()
     {
-        axleInfos.Add(new AxleInfo(wheels_Col[0], wheels_Col[1], wheels_Col[2], wheels_Col[3], true, true));
-        axleInfos.Add(new AxleInfo(wheels_Col[4], wheels_Col[5], wheels_Col[6], wheels_Col[7], true, false));
+        axleInfos.Add(new AxleInfo(wheels_Col[0], wheels_Col[1], false, true));
+        axleInfos.Add(new AxleInfo(wheels_Col[2], wheels_Col[3], true, false));
     }
 }
--- Assets/3.Script/Player/PlayerControl.cs
+++ Assets/3.Script/Player/PlayerControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
@@ -8,16 +9,24 @@ public class PlayerControl : MonoBehaviour
     [Header("Player")]
     public Kart kart;
     private float targetSpeed = 0;
+    private float currentSpeed = 0;
+
+    [Header("Status")]
+    // 차량의 회전 반지름
+    public float radius = 600;
+    // 속도에 따른 압력 증가치
+    public float downForce = 1000.0f;
 
     [Header("State")]
     private PlayerState currentStarategy = new NormalMoveState();
     public bool isBoost { get; private set; }
 
     [Header("Components")]
+    public PlayerInput input;
     private Animator anim;
     private Rigidbody rigid;
-
-    public PlayerInput input;
+    [SerializeField]
+    private Text speed_txt;
 
     private void Awake()
     {
@@ -30,26 +39,29 @@ public class PlayerControl : MonoBehaviour
     private void Start()
     {
         // 안정성 위해 무게중심 밑으로 설정
-        rigid.centerOfMass = 0.3f * Vector3.down;
+        //rigid.centerOfMass = Vector3.down;
+
+        temp = kart.frontWheelSideFriction;
     }
 
     private void FixedUpdate()
     {
+        //Drift();
         Move();
-        Drift();
         WheelPos();
-        AddDownForce();
+        UpdateSpeed();
+        //AddDownForce();
     }
 
     #region 이동
-    public float radius = 6;
     private void Move()
     {
         // 속도 계산
-        if (Mathf.Abs(targetSpeed) < kart.maxSpeed)
-        {
-            targetSpeed += kart.speed * Time.deltaTime * input.move.y;
-        }
+        //if (Mathf.Abs(targetSpeed) < kart.maxSpeed)
+        //{
+        //    targetSpeed += kart.speed * Time.deltaTime * input.move.y;
+        //}
+        targetSpeed = kart.speed * input.move.y;
         if (input.move.Equals(Vector2.zero))
         {
             targetSpeed = 0;
@@ -68,22 +80,77 @@ public class PlayerControl : MonoBehaviour
             // 바퀴 회전 (애커만 스티어링)
             if (axleInfo.steering)
             {
-                for (int i = 0; i < 2; i++)
-                {
-                    axleInfo.leftWheel[i].steerAngle = Mathf.Rad2Deg * Mathf.Atan(kart.wheelBase / (radius + halfVehicleWidth * input.move.x)) * steering;
-                    axleInfo.rightWheel[i].steerAngle = Mathf.Rad2Deg * Mathf.Atan(kart.wheelBase / (radius - halfVehicleWidth * input.move.x)) * steering;
-                }
+                axleInfo.leftWheel.steerAngle = Mathf.Rad2Deg * Mathf.Atan(kart.wheelBase / (radius + halfVehicleWidth * input.move.x)) * steering;
+                axleInfo.rightWheel.steerAngle = Mathf.Rad2Deg * Mathf.Atan(kart.wheelBase / (radius - halfVehicleWidth * input.move.x)) * steering;
             }
             // 바퀴 굴림
             if (axleInfo.motor)
             {
-                axleInfo.leftWheel[0].motorTorque = targetSpeed;
-                axleInfo.leftWheel[1].motorTorque = targetSpeed;
-                axleInfo.rightWheel[0].motorTorque = targetSpeed;
-                axleInfo.rightWheel[1].motorTorque = targetSpeed;
+                axleInfo.leftWheel.motorTorque = targetSpeed;
+                axleInfo.rightWheel.motorTorque = targetSpeed;
+                Debug.Log(axleInfo.leftWheel.rpm);
+                Debug.Log(axleInfo.rightWheel.rpm);
+
             }
         }
     }
+    private void UpdateSpeed()
+    {
+        currentSpeed = rigid.velocity.magnitude * 3.6f;
+        if(currentSpeed < 0.9f)
+        {
+            currentSpeed = 0;
+        }
+        speed_txt.text = currentSpeed.ToString("F1");
+    }
+    private void Drift()
+    {
+        if (input.drift)
+        {
+            kart.axleInfos[1].leftWheel.forwardFriction = kart.driftFriction_f;
+            kart.axleInfos[1].leftWheel.sidewaysFriction = kart.driftFriction_s;
+            kart.axleInfos[1].rightWheel.forwardFriction = kart.driftFriction_f;
+            kart.axleInfos[1].rightWheel.sidewaysFriction = kart.driftFriction_s;
+        }
+        else
+        {
+            kart.axleInfos[1].leftWheel.forwardFriction = kart.nomalFriction_f;
+            kart.axleInfos[1].leftWheel.sidewaysFriction = kart.nomalFriction_s;
+            kart.axleInfos[1].rightWheel.forwardFriction = kart.nomalFriction_f;
+            kart.axleInfos[1].rightWheel.sidewaysFriction = kart.nomalFriction_s;
+        }
+    }
+    #endregion 이동
+
+    #region 차량 안정화
+    WheelFrictionCurve temp;
+    private void AddDownForce()
+    {
+        float force = downForce * rigid.velocity.magnitude;
+        rigid.AddForce(-transform.up * force);
+
+        //for (int i = 0; i < 2; i++)
+        //{
+        //    if (force != 0)
+        //    {
+        //        temp.extremumSlip *= (1 + (force / rigid.mass));
+        //        temp.asymptoteSlip *= (1 + (force / rigid.mass));
+        //        kart.axleInfos[0].leftWheel.sidewaysFriction = temp;
+        //        temp.extremumSlip *= (1 + (force / rigid.mass));
+        //        temp.asymptoteSlip *= (1 + (force / rigid.mass));
+        //        kart.axleInfos[0].rightWheel.sidewaysFriction = temp;
+        //    }
+        //    else
+        //    {
+        //        temp = kart.frontWheelSideFriction;
+        //        kart.axleInfos[0].leftWheel.sidewaysFriction = kart.frontWheelSideFriction;
+        //        kart.axleInfos[0].rightWheel.sidewaysFriction = kart.frontWheelSideFriction;
+        //    }
+        //}
+    }
+    #endregion
+
+
     /// <summary>
     /// 바퀴 이미지를 휠 콜라이더 위치랑 동기화 시키는 메소드
     /// </summary>
@@ -94,42 +161,9 @@ public class PlayerControl : MonoBehaviour
 
         for (int i = 0; i < 4; i++)
         {
-            Vector3 temp1, temp2;
-            kart.wheels_Col[i * 2].GetWorldPose(out temp1, out wheelRotation);
-            kart.wheels_Col[i * 2 + 1].GetWorldPose(out temp2, out wheelRotation);
-            wheelPosition = (temp1 + temp2) * 0.5f;
+            kart.wheels_Col[i].GetWorldPose(out wheelPosition, out wheelRotation);
             kart.wheels_Mesh[i].transform.position = wheelPosition;
             kart.wheels_Mesh[i].transform.rotation = wheelRotation;
         }
     }
-    private void Drift()
-    {
-        for (int i = 0; i < 2; i++)
-        {
-            if (input.drift)
-            {
-                kart.axleInfos[1].leftWheel[i].forwardFriction = kart.driftFriction_f;
-                kart.axleInfos[1].leftWheel[i].sidewaysFriction = kart.driftFriction_s;
-                kart.axleInfos[1].rightWheel[i].forwardFriction = kart.driftFriction_f;
-                kart.axleInfos[1].rightWheel[i].sidewaysFriction = kart.driftFriction_s;
-            }
-            else
-            {
-                kart.axleInfos[1].leftWheel[i].forwardFriction = kart.nomalFriction_f;
-                kart.axleInfos[1].leftWheel[i].sidewaysFriction = kart.nomalFriction_s;
-                kart.axleInfos[1].rightWheel[i].forwardFriction = kart.nomalFriction_f;
-                kart.axleInfos[1].rightWheel[i].sidewaysFriction = kart.nomalFriction_s;
-            }
-        }
-    }
-    #endregion 이동
-
-    #region 차량 안정화
-    public float downForce = 100.0f;
-
-    private void AddDownForce()
-    {
-        rigid.AddForce(-transform.up * downForce * rigid.velocity.magnitude);
-    }
-    #endregion
 }
