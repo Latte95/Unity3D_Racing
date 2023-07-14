2de7b2a202ff999aac4b1b6787cece08da1961c9
Latte95 <123979439+Latte95@users.noreply.github.com>
2023-07-14 16:45:15 +0900

    Add_미니맵 위치 갱신

---
+++ Assets/3.Script/Map/Minimap.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Minimap : MonoBehaviour
+{
+    public RectTransform minimapStart;
+    public RectTransform minimapEnd;
+    public Transform normalStart;
+    public Transform normalEnd;
+    public Image icon;
+
+    private float normalDistance;
+    private float minimapDistance;
+
+    private void Awake()
+    {
+        normalDistance = Vector3.Distance(normalEnd.position, normalStart.position);
+        minimapDistance = Vector3.Distance(minimapEnd.position, minimapStart.position);
+    }
+    private void Update()
+    {
+        float proportionX = -transform.position.x / normalDistance;
+        float proportionY = -transform.position.z / normalDistance;
+
+        Vector3 minimapPosition = new Vector3(
+            proportionX * minimapDistance,
+            proportionY * minimapDistance,
+            0
+        );
+
+        icon.rectTransform.anchoredPosition = minimapPosition;
+    }
+}
